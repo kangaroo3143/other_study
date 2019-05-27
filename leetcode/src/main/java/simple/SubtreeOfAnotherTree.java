c948fb38a434c397c837a810fd4852873fe7f836
17756093143 <375382674@qq.com>
2019-05-27 18:06:15 +0800

    572. Subtree of Another Tree

---
+++ leetcode/src/main/java/simple/SubtreeOfAnotherTree.java
@@ -0,0 +1,68 @@
+/**
+ * Copyright (c),2016-2018, iThinkDT
+ * <br/>This program is protected by copyright laws;
+ * <br/>Program Name: other_study<b>ithinkdt<b>
+ * <br/>
+ * Package: simple <br <br/>
+ * FileName: IsSubtree<br <br/>
+ * <br/>
+ * <br/>
+ * <br/>
+ *
+ * @author ithinkdt
+ * @version 1.0
+ * @since JDK 1.8
+ */
+package simple;
+
+/**
+ * * com.ithinkdt.dqa.admin.config
+ * <p>572. Subtree of Another Tree
+ * 给定两个非空二叉树 s 和 t，检验 s 中是否包含和 t 具有相同结构和节点值的子树。s 的一个子树包括 s 的一个节点和这个节点的所有子孙。s 也可以看做它自身的一棵子树。
+ * <p>
+ * 示例 1:
+ * 给定的树 s:
+ * <p>
+ * 3
+ * / \
+ * 4   5
+ * / \
+ * 1   2
+ * 给定的树 t：
+ * <p>
+ * 4
+ * / \
+ * 1   2
+ *
+ * @version 1.0
+ * @descreption:
+ * @date:2019/5/27 17:56
+ * @author: liubo
+ * @since JDK1.8
+ */
+public class SubtreeOfAnotherTree {
+
+
+
+
+    public class TreeNode {
+        int val;
+        TreeNode left;
+        TreeNode right;
+
+        TreeNode(int x) {
+            val = x;
+        }
+    }
+
+
+    public boolean isSubtree(TreeNode s, TreeNode t) {
+
+        //todo
+        return false;
+    }
+
+
+
+
+}
